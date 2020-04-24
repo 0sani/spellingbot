d85b742140dd8da6b84392d7ef19b3818f70331d
0sani <49707036+0sani@users.noreply.github.com>
2020-04-23 17:02:54 -0700

    Add files via upload

---
+++ discordBot.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+import random
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print('Bot is ready')
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined a server')
+
+@client.command(aliases=['emoji'])
+async def cursed_emoji(ctx):
+    await ctx.send(file=discord.File('cursedKevinImage.png'))
+
+@client.command(aliases=['8ball'])
+async def _8ball(ctx,*, question):
+    responses = [
+        'It is certain',
+        'Most Likely',
+        'Possibly',
+        'No',
+        'Definitely not'
+        ]
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+
+@client.command()
+async def spelling(ctx):
+    x= await ctx.channel.history(limit=2).flatten()
+    message=x[1].content
+    sarcasm = ''
+    for i in range(len(message)):
+        if i % 2 == 0:
+            sarcasm += message[i].upper()
+        else:
+            sarcasm += message[i].lower()
+    print(sarcasm)
+
+    await ctx.send(sarcasm)
+
+
+client.run('NjkzOTM4MzM4NjMzMDg5MTg2.XoEXMA.ae2h7IDoLxxGcfyiU7Lh4x4MBnA')
